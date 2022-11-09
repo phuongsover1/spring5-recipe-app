d80ed4c66cfd08de839c96f24dc8f6d93672e6ff
Phuong Nguyen <phuongsover2@gmail.com>
2022-11-09 13:04:36 +0700

    @OneToOne to UnitOfMeasure Entity.

--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -7,6 +7,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Ingredient {
@@ -20,6 +21,9 @@ public class Ingredient {
 
   @ManyToOne
   private Recipe recipe;
+
+  @OneToOne
+  private UnitOfMeasure uom;
 
   public Long getId() {
     return id;
@@ -51,6 +55,14 @@ public class Ingredient {
 
   public void setRecipe(Recipe recipe) {
     this.recipe = recipe;
+  }
+
+  public UnitOfMeasure getUom() {
+    return uom;
+  }
+
+  public void setUom(UnitOfMeasure uom) {
+    this.uom = uom;
   }
 
 }
